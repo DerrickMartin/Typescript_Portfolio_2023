f0c0debfd2c4269025fc779068941593832b5456
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-05-04 19:45:09 +0000

    Added browser router

---
+++ portfolio/src/components/navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <ul className="navbar">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/skills" className="nav-link">Skills</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/interest" className="nav-link">Interest</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+  
