14ba82f6fe2b1485e7f2543d3abd49bb7837d3d7
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-05-11 04:52:20 +0000

    Added a form layout

---
+++ portfolio/src/components/email.jsx
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import './email.css';
+
+export const ContactUs = () => {
+  const form = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xokj9ke', 'template_85sfsjw', form.current, 'F4Hz-esOsqEL9dYZ5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  const openForm = () => {
+    setIsOpen(true);
+  };
+
+  const closeForm = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      {!isOpen && <button onClick={openForm}>Open Form</button>}
+      {isOpen && (
+        <div className="form-overlay">
+          <form ref={form} onSubmit={sendEmail} className="form-container">
+            <button className="close-button" onClick={closeForm}>
+              X
+            </button>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+
+
--- portfolio/src/pages/Home/index.tsx
+++ portfolio/src/pages/Home/index.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
-import emailjs from '@emailjs/browser';
+
+import {ContactUs} from '../../components/email';
 
 //HeroSection
 import Hero from '../../components/Home/Hero';
 
 const Home = () => {
 
-  const sendEmail = async () => {
-    try {
-      const response = await emailjs.send(
-        'service_xokj9ke',
-        'template_85sfsjw',
-        { /* Email template variables */ },
-        'F4Hz-esOsqEL9dYZ5'
-      );
-      console.log('Email sent successfully:', response.status);
-    } catch (error) {
-      console.error('Error sending email:', error);
-    }
-  };
+  
     return (
       <div>
         <Hero />
         <h1>Home Page</h1>
         <p>Welcome to my website!</p>
         <div>
-        <button onClick={sendEmail}>Send Email</button>
+        <ContactUs />
         </div>
       </div>
     );
