4fd3496bfd03ad53236e82369e78cd4f2505f9af
Derrick Martin <martin.derrick294@yahoo.com>
2023-05-29 15:05:45 -0700

    Updated Skill page

--- portfolio/src/components/Skills/Hero.tsx
+++ portfolio/src/components/Skills/Hero.tsx
@@ -4,9 +4,20 @@ import './Hero.css';
 const SkillsHero: React.FC = () => {
   return (
     <section className="hero">
-      <div className="hero-content">
+      <div className="heroContent">
         <h1>My skillset</h1>
         <p>Lets keep learning.</p>
+        <div className='skillCategory'>
+          <div className="skillBox">
+            <h3>FrontEnd</h3>
+          </div>
+          <div className="skillBox">
+            <h3>BackEnd</h3>
+          </div>
+          <div className="skillBox">
+            <h3>DevOps</h3>
+          </div>
+        </div>
       </div>
     </section>
   );
