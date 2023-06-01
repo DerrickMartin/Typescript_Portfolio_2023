d7c655ea6ed90337eda602a9e147c788d53dd0c1
Derrick Martin <martin.derrick294@yahoo.com>
2023-05-31 22:02:53 -0700

    Updated skills page

--- portfolio/src/components/Skills/Hero.tsx
+++ portfolio/src/components/Skills/Hero.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Hero.css';
-import { useState } from 'react';
+import devOpsSvg from '../../assets/svgs/cloud/devops_logo.svg';
+import jsSvg from '../../assets/svgs/javascript_logo.svg';
+import pythonSvg from '../../assets/svgs/backend/python_logo.svg';
 
 const SkillsHero: React.FC = () => {
   const [clickedElements, setClickedElements] = useState<number[]>([]);
@@ -14,20 +16,57 @@ const SkillsHero: React.FC = () => {
       }
     });
   };
+
   return (
     <section className="hero">
       <div className="heroContent">
         <h1>My skillset</h1>
         <p>Lets keep learning.</p>
-        <div className='skillCategory'>
+        <div className="skillCategory">
           <div className="skillBox">
-          <h3 className={`skillSection ${clickedElements.includes(0) ? 'clicked' : ''}`} onClick={() => handleClick(0)}>FrontEnd</h3>
+            <h3
+              className={`skillSection ${clickedElements.includes(0) ? 'clicked' : ''}`}
+              onClick={() => handleClick(0)}
+            >
+              FrontEnd
+            </h3>
+            <div className={`DevOps ${clickedElements.includes(0) ? 'visible' : ''}`}>
+              <img
+              className={`python ${clickedElements.includes(0) ? 'visible' : ''}`}
+              src={pythonSvg}
+              alt="Python"
+              style={{ width: '52px', height: '52px' }}/>
+            </div>
           </div>
           <div className="skillBox">
-          <h3 className={`skillSection ${clickedElements.includes(1) ? 'clicked' : ''}`} onClick={() => handleClick(1)}>BackEnd</h3>
+            <h3
+              className={`skillSection ${clickedElements.includes(1) ? 'clicked' : ''}`}
+              onClick={() => handleClick(1)}
+            >
+              BackEnd
+            </h3>
+            <div className={`DevOps ${clickedElements.includes(1) ? 'visible' : ''}`}>
+              <img
+              className={`python ${clickedElements.includes(1) ? 'visible' : ''}`}
+              src={jsSvg}
+              alt="Javascript"
+              style={{ width: '52px', height: '52px' }}/>
+            </div>
           </div>
           <div className="skillBox">
-          <h3 className={`skillSection ${clickedElements.includes(2) ? 'clicked' : ''}`} onClick={() => handleClick(2)}>DevOps</h3>
+            <h3
+              className={`skillSection ${clickedElements.includes(2) ? 'clicked' : ''}`}
+              onClick={() => handleClick(2)}
+            >
+              DevOps
+            </h3>
+            <div className={`DevOps ${clickedElements.includes(2) ? 'visible' : ''}`}>
+              <img
+              className={`python ${clickedElements.includes(2) ? 'visible' : ''}`}
+              src={devOpsSvg}
+              alt="DevOps"
+              style={{ width: '52px', height: '52px' }}/>
+            </div>
           </div>
         </div>
       </div>
