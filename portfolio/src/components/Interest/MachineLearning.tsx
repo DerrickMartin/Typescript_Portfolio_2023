30e8692eddca1ce11706c2c6eb8214a952d85767
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-06-19 06:56:18 +0000

    Updated carosel size

--- portfolio/src/components/Interest/MachineLearning.tsx
+++ portfolio/src/components/Interest/MachineLearning.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import 'MachineLearning.css';
 import MachineLearningProjects from "./Projects/MachineLearningProjects";
+
 
 const MachineLearning: React.FC = () => {
   return (
-    <div>
+    <div className="container">
       <h2>Machine Learning</h2>
       <p>Here are a few examples of Machine Learning</p>
       <MachineLearningProjects />
