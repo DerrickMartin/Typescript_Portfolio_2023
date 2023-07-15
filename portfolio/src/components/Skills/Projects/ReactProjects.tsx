0c2d97ea6df430da1cdc386fdb0181588fefdb91
Derrick Martin <30300627+DerrickMartin@users.noreply.github.com>
2023-07-15 17:36:10 +0000

    Added First model for projects

--- portfolio/src/components/Skills/Projects/ReactProjects.tsx
+++ portfolio/src/components/Skills/Projects/ReactProjects.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import './ProjectsStyle.css';
 import ReactSvg from '../../../assets/svgs/react_logo.svg';
 
 
 const ReactProjects: React.FC = () => {
+  const [showModule, setShowModule] = useState(false);
+
+  const handleClick = () => {
+    setShowModule(!showModule);
+  };
   return (
     <div className="caroselContainer">
         <img
@@ -11,7 +16,18 @@ const ReactProjects: React.FC = () => {
           src={ReactSvg}
           alt="React Svg"/>
       <div id="container">
-        <div className="square"></div>
+        <div className="square" onClick={handleClick}></div>
+        {showModule && (
+        <div className="opacityBackground" onClick={handleClick}>
+        <div className="reactProject1">
+          <button className="closeButton" onClick={handleClick}>
+            X
+          </button>
+          <h2>Module Content</h2>
+          <p>This is the module content.</p>
+        </div>
+        </div>
+      )}
         <div className="square"></div>
         <div className="square"></div>
         <div className="square"></div>
